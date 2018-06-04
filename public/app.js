1ac6290cb6998baa1b58504b7ee879fb39176913
Kyle Weingart <kyle.weingart@gmail.com>
2018-06-03 23:57:48 -0400

    creating bootstrap cards to hold scraped articles

--- public/app.js
+++ public/app.js
@@ -1,10 +1,39 @@
+function displayResults(articles) {
+  // First, empty the table
+  $("#articles").empty();
+  console.log("cool");
+  // Then, for each entry of that json...
+  articles.forEach(function(article) {
+    // Append each of the animal's properties to the table
+    $("#articles").append("<div class=card>");
+    $("#articles").append("<img class=card-img-top src=liverbird.png alt=Card image cap>");
+    $("#articles").append("<div class=card-body>");
+    $("#articles").append("<h5 class=card-title>" + article.title + "</h5");
+    $("#articles").append("<p class=card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</p>");
+    $("#articles").append("<a href=# class=btn btn-primary>Save</a>");
+    $("#articles").append("</div>");
+    $("#articles").append("</div>");
+
+
+
+
+
+    
+    
+    
+   
+  });
+}
+
 // Grab the articles as a json
 $.getJSON("/articles", function(data) {
     // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    }
+    // for (var i = 0; i < data.length; i++) {
+    //   // Display the apropos information on the page
+    //   $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+  // }
+    displayResults(data);
+    
   });
   
   
